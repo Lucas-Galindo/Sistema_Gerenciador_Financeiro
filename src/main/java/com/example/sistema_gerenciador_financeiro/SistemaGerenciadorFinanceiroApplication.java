7d3d1c5439cb517b3d13e6b708ca378124080ee2
Lucas Marçon <srlucasmtg@gmail.com>
2026-02-25 21:30:18 -0300

    salvando

---
+++ src/main/java/com/example/sistema_gerenciador_financeiro/SistemaGerenciadorFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_gerenciador_financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciadorFinanceiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaGerenciadorFinanceiroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistema_gerenciador_financeiro/SistemaGerenciadorFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_gerenciador_financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciadorFinanceiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
